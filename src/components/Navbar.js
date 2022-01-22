35022621e78ea6a5955945f0451ca409ccebecb2
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-22 09:38:56 -0600

    Adding Categories to SideBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { FaBars } from 'react-icons/fa'
 import logo from '../assets/logo.svg'
 import Links from '../constants/links'
 import SocialLinks from '../constants/socialLinks'
+
 const Navbar = ({toggle}) => {
   return (
     <nav className='navbar'>
