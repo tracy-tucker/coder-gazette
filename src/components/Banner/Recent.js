e95573b09f1b24caf78d5c00d483072ace07c99a
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-21 19:15:35 -0600

    Querying DB to grab 5 recent posts for Recent Banner

--- src/components/Banner/Recent.js
+++ src/components/Banner/Recent.js
@@ -5,13 +5,52 @@ import { Link } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Title from './Title'
 
-
+export const query = graphql`
+  {
+    allMdx(limit: 5, sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const Recent = () => {
+  const data = useStaticQuery(query)
+  const {
+    allMdx:{nodes:posts},
+  } = data
   
   return (
     <Wrapper>
-      Banner Recent
+      <Title title="recent posts" />
+      {posts.map((post) => {
+        const {title, slug, date, image} = post.frontmatter;
+        return <Link to={`/posts/${slug}`} key={post.id} className='post'>
+          <GatsbyImage
+            image={getImage(image)}
+            alt={title}
+            className='img'
+          />
+          <div>
+            <h5>{title}</h5>
+            <p>{date}</p>
+          </div>
+        </Link>
+      })}
     </Wrapper>
   )
 }
