3dd5498e84d69f3c20f71820b39f20fb03a57afb
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-22 14:03:55 -0600

    Testing heading styles within root.mdx

--- root-mdx.js
+++ root-mdx.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 
+const myH2 = props => {
+    console.log(props)
+    if(props.title){
+        return (
+            <h2 {...props} style={{fontSize:"2rem",color:"red"}}>
+                {props.children}
+            </h2>
+        )
+    }
+    return (
+        <h2 {...props} className='code'>
+            {props.children}
+        </h2>
+    )
+}
+
 const components = {
-    // all logic
+    h2: myH2,
 }
 
 export const wrapMDX = ({element}) => {
     return (
-        <MDXProvider>
-            components={components}
+        <MDXProvider components={components}>
             {element}
         </MDXProvider>
     )
