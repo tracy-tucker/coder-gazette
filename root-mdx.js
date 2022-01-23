a93a6beae3d180b9e34a2b93309241132ab62ac6
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-23 12:43:43 -0600

    Configuring example component to style code blocks

--- root-mdx.js
+++ root-mdx.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import {Headings, Code, Blockquote} from './src/components/Complete'
+import {Headings, Code, Blockquote, Example} from './src/components/Complete'
 
 const components = {
     h2: Headings.myH2,
     h4: Headings.myH4,
     inlineCode: Code,
     blockquote: Blockquote,
+    pre: Example,
 }
 
 export const wrapMDX = ({element}) => {
