55452063f5b69c22e6cc72d87abb9ae511d058b1
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-23 11:45:45 -0600

    Adding Code component to root-mdx.js

--- root-mdx.js
+++ root-mdx.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 import {Headings} from './src/components/Complete'
 
+const Code = ({children}) => {
+    return <code className='code'>{children}</code>
+}
 
 const components = {
     h2: Headings.myH2,
     h4: Headings.myH4,
+    inlineCode: Code,
 }
 
 export const wrapMDX = ({element}) => {
