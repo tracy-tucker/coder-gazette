d44c52066520ea2e74ccefc022a51aa40116406b
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-23 12:09:57 -0600

    Adding blockquotesto root-mdx.js

--- root-mdx.js
+++ root-mdx.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import {Headings} from './src/components/Complete'
-
-const Code = ({children}) => {
-    return <code className='code'>{children}</code>
-}
+import {Headings, Code, Blockquote} from './src/components/Complete'
 
 const components = {
     h2: Headings.myH2,
     h4: Headings.myH4,
     inlineCode: Code,
+    blockquote: Blockquote,
 }
 
 export const wrapMDX = ({element}) => {
